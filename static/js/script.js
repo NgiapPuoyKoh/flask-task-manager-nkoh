48cd5cf12d6f3622f30d81b7689a1cd1ee9707c2
NgiapPuoyKoh <ngiappuoykoh@gmail.com>
2020-10-13 11:40:57 +0000

    Add Materialize Navbar and Sidenav

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+// initialize side navbar
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   var elems = document.querySelectorAll(".sidenav");
+//   var instances = M.Sidenav.init(elems, options);
+// });
+
+$(document).ready(function () {
+  $(".sidenav").sidenav({ edge: "right" });
+});
