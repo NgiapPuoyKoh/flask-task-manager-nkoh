174944e1bebd3359c67926b027b5ad9425d561d3
NgiapPuoyKoh <ngiappuoykoh@gmail.com>
2020-10-14 13:53:15 +0000

    Add Login Functionality

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
+# from os import path
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
+
 if os.path.exists("env.py"):
     import env
 
@@ -14,7 +16,9 @@ app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.secret_key = os.environ.get("SECRET_KEY")
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 mongo = PyMongo(app)
 
@@ -28,10 +32,59 @@ def get_tasks():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        # CHALLENGE: cusomize the hash and salt mthods to own preference
+        # To include a secondary password file to confirm the users password
+        # do it priot to creating the user dictionary
+
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        # using session function imported
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful")
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if user name exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # invalid password match
+                flash("Incorrect username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-            port = int(os.environ.get("PORT")),
-            debug = True)
+            port=int(os.environ.get("PORT")),
+            debug=True)
